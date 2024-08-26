e3a0128f58075aa58483e498c9578d73b7562fe0
hitters07 <webhitters@gmail.com>
2024-08-26 18:18:32 +0530

    updated

--- home-javascript.js
+++ home-javascript.js
@@ -220,3 +220,21 @@ questions.forEach((question, index) => {
     }
   });
 }); 
+// ==================pop-up-form==============
+function showPopup() {
+  const popup = document.getElementById('popup1');
+  popup.style.display = 'flex';
+  popup.classList.add('show');
+}
+
+function closePopup() {
+  const popup = document.getElementById('popup1'); 
+  popup.classList.remove('show');
+  setTimeout(() => {
+    popup.style.display = 'none'; 
+  }, 300);
+} 
+//  console.log(Date())
+setTimeout(showPopup, 10000);
+
+
